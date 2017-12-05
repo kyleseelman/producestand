32384d408715c72db76466db6230d2e7bb7449a4
lindafang70 <32524278+lindafang70@users.noreply.github.com>
2017-12-05 15:37:32 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,1 +1,36 @@
 #include "ProduceStand.h"
+
+int main(int argc, char* argv[]){
+  ifstream input;
+  ofstream receiptOut;
+  ofstream customers;
+  ofstream infoStats;
+  string type;
+  char choice;
+  int count=0;
+  
+  while(!input.eof()){
+    input >> type;
+    
+    if(type=="Vegetable"){
+      Vegetable v;
+      v(input,receiptOut);
+    }
+    else if(type=="Fruit"){
+      Fruit f;
+      f(input,receiptOut);
+    }
+    else{
+      cout << "Error. Unknown type\n";
+    }
+    
+    input >> choice;
+    if(choice=='y'){
+      Person p;
+      count++;
+      p(input,customers);
+    } 
+  }
+  
+  return 0;
+}
