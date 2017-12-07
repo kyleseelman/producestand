fc42ce381140bc1e17d8efe209fd5cb93d1c0b67
Kyle Seelman <33760745+kyleseelman@users.noreply.github.com>
2017-12-07 13:12:49 -0500

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,9 +1,11 @@
 #include "Date.h"
 #include "ProduceStand.h"
 
+//Constructors
 Date::Date():month(0),day(0),year(0){}
 Date::~Date(){}
-      
+
+//implementation of the setters
 void Date::setMonth(int m){
   month = m;
 }
@@ -14,6 +16,7 @@ void Date::setYear(int y){
   year = y;
 }
 
+//implementation of the getters
 int Date::getMonth(){
   return month;
 }
@@ -24,6 +27,7 @@ int Date::getYear(){
   return year;
 }
 
+//print function for the date variables
 void Date::printDate(ostream& out){
   out << month << "/" << day <<"/" << year << endl<<endl;
 }
