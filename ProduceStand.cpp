09654828b2ba8f4374aea369d45f7413b4c92da1
Kyle Seelman <33760745+kyleseelman@users.noreply.github.com>
2017-12-07 13:42:43 -0500

    Update ProduceStand.cpp

--- ProduceStand.cpp
+++ ProduceStand.cpp
@@ -1,9 +1,11 @@
 #include "ProduceStand.h"
 
+//initializaiton of the static variables 
 int ProduceStand::receiptNum=1;
 double ProduceStand::dailySales=0;
 double ProduceStand::dailySalesTax=0;
 
+//constructors
 ProduceStand::ProduceStand():name(" "), weight(0),pricePerlb(0), 
               pricePerEach(0),howMany(0){}
 
@@ -11,29 +13,29 @@ ProduceStand::ProduceStand(string n,double w,double ppw,double ppe,int num):
               name(n),weight(w),pricePerlb(ppw),pricePerEach(ppe),howMany(num){
   //receiptNum++;
 }
-
+//destructor
 ProduceStand::~ProduceStand(){}
 
+//initialization of the setters
 void ProduceStand::setStandName(string name){
   standName=name;
 }
 
+//initialization of the getters
 string ProduceStand::getStandName(){
   return standName;
 }
-
 int ProduceStand::getReceiptNum(){
   return receiptNum;
 }
-
 double ProduceStand::getDailySales(){
   return dailySales;
 }
-
 double ProduceStand::getDailySalesTax(){
   return dailySalesTax;
 }
 
+//print function for the header of the receipts
 void ProduceStand::printHeader(ofstream& out){
   standName="Hey Y'all";
 
@@ -49,6 +51,7 @@ void ProduceStand::printHeader(ofstream& out){
   out << endl << endl;
 }
 
+//print function for the producestand variables
 void ProduceStand::printStats(ofstream& out){
   out << "Total sales for the day: $" << ProduceStand::getDailySales()
       << "\nTotal sales tax collected for the day: $" 
