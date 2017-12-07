859d53ca9ede1e793d8efa852e6d22af29231eef
Kyle Seelman <33760745+kyleseelman@users.noreply.github.com>
2017-12-07 14:38:20 -0500

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,7 +1,16 @@
+/*
+  Linda Fang and Kyle Seelman
+  CPSC 1020 
+  Programming Assingment 4
+  kseelma and fang4
+  Dr. Feaster
+*/
+
 #ifndef DATE_H
 #define DATE_H
 #include <iostream>
 using namespace std;
+
 
 class Date
 {
@@ -11,14 +20,16 @@ class Date
     int year;
 
   public:
+    //constructors
     Date();
     Date(int m,int d,int y):month(m),day(d),year(y){}
+    //destructors
     ~Date();
-      
+    //setters
     void setMonth(int);
     void setDay(int);
     void setYear(int);
-
+    //getters
     int getMonth();
     int getDay();
     int getYear();
