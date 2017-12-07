cd15c45b9bb8a48896ffd6c997fd9b9782b779fd
Kyle Seelman <33760745+kyleseelman@users.noreply.github.com>
2017-12-07 14:47:09 -0500

    Update Fruit.h

--- Fruit.h
+++ Fruit.h
@@ -1,3 +1,12 @@
+/*
+  Linda Fang and Kyle Seelman
+  CPSC 1020 
+  Programming Assingment 4
+  kseelma and fang4
+  Dr. Feaster
+*/
+
+
 #ifndef FRUIT_H
 #define FRUIT_H
 
@@ -6,22 +15,27 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
+
 class Fruit : public ProduceStand
 {
   private:
-
+    //static variables
     static double totalFruitSales;
     static int fruitCount;
 
   public:
-
+    //constructors
     Fruit();
-    ~Fruit(){}
     Fruit(ifstream&, ofstream& out);
     Fruit(string, double, double, double, int);
+    //destructor
+    ~Fruit(){}
+    
+    //implementation of getters
     static double getTotalFruitSales(){return totalFruitSales;}
     static int getfruitCount(){return fruitCount;}
 
+    //functions to calculate tax, sale, and print receipt
     virtual void calculateTax(ofstream& out);
     virtual void calculateSale(ofstream& out);
     virtual void printReceipt(ofstream& out);
