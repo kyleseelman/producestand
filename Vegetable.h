eef84a9ae024fe01f1e5a70ddae6ee13ec8cd955
Kyle Seelman <33760745+kyleseelman@users.noreply.github.com>
2017-12-07 15:01:39 -0500

    Update Vegetable.h

--- Vegetable.h
+++ Vegetable.h
@@ -1,3 +1,12 @@
+/*
+ Linda Fang and Kyle Seelman
+ CPSC 1020
+ Programming Assingment 4
+ kseelma and fang4
+ Dr. Feaster
+*/
+
+
 #ifndef VEGETABLE_H
 #define VEGETABLE_H
 
@@ -19,13 +28,16 @@ class Vegetable : public ProduceStand
     Vegetable():ProduceStand(){};
     Vegetable(ifstream& in, ofstream& out);
     Vegetable(string, double, double, double, int);
+    //destructor
     virtual ~Vegetable() {}
 
+    //functions to calculate taxe and sale total
     virtual void calculateTax(ofstream& out);
     virtual void calculateSale(ofstream& out);
     virtual void printReceipt(ofstream& out);
     void printInfo(ofstream& out);
 
+    //getters for static variables
     static double getTotalVegtSales();
     static int getVegtCount();
 };
